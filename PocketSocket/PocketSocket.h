77b36e6ce5be40846da61867b32b8fc3cd3a714a
Justin Makaila <justinmakaila@gmail.com>
2015-05-28 11:01:13 -0400

    Added carthage support

---
+++ PocketSocket/PocketSocket.h
@@ -0,0 +1,22 @@
+//
+//  PocketSocket.h
+//  PocketSocket
+//
+//  Created by Justin Makaila on 5/28/15.
+//  Copyright (c) 2015 Zwopple Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestTarget.
+FOUNDATION_EXPORT double PocketSocketVersionNumber;
+
+//! Project version string for TestTarget.
+FOUNDATION_EXPORT const unsigned char PocketSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestTarget/PublicHeader.h>
+
+#import "PSWebSocket.h"
+#import "PSWebSocketServer.h"
+#import "PSWebSocketDriver.h"
+#import "PSWebSocketTypes.h"
